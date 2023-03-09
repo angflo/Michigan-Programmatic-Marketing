67c02f0e6918e825fa0b9f04719660773cc35afe
alecpo <alecpo@umich.edu>
2023-03-09 14:41:10 -0500

    Change file under YourBranch

---
+++ CircleAreaCalculator.java
@@ -0,0 +1,8 @@
+public class CircleAreaCalculator {
+    public static void main(String[] args) {
+        double radius = 5.0; // radius of the circle
+        double area = Math.PI * radius * radius; // formula for area of a circle
+        System.out.println("The area of the circle is " + area);
+        System.out.println("I hope I do well on this exam!");
+    }
+}
